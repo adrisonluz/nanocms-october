21158c60935652a73899aebb37931ab07f79214a
Adrison Luz <aluz@reweb.com.br>
2017-01-22 01:12:11 -0200

    Commit inicial

---
+++ Plugin.php
@@ -0,0 +1,14 @@
+<?php namespace AdrisonLuz\Nanocms;
+
+use System\Classes\PluginBase;
+
+class Plugin extends PluginBase
+{
+    public function registerComponents()
+    {
+    }
+
+    public function registerSettings()
+    {
+    }
+}
---
+++ lang/en/lang.php
@@ -0,0 +1,6 @@
+<?php return [
+    'plugin' => [
+        'name' => 'NanoCms',
+        'description' => 'Nano CMS é um gerenciador de conteúdo simples e prático, onde você pode criar, editar, excluir, gerenciar completamente o conteúdo do seu site.'
+    ]
+];
