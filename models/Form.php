acdb3dc92be8a703ec0cd5e726273799411ed14d
Adrison Luz <aluz@reweb.com.br>
2017-01-22 17:54:00 -0200

    Criado Models

---
+++ models/Form.php
@@ -0,0 +1,32 @@
+<?php namespace AdrisonLuz\NanoCms\Models;
+
+use Model;
+
+/**
+ * Model
+ */
+class Form extends Model
+{
+    use \October\Rain\Database\Traits\Validation;
+
+    /*
+     * Validation
+     */
+    public $rules = [
+    ];
+
+    /*
+     * Disable timestamps by default.
+     * Remove this line if timestamps are defined in the database table.
+     */
+    public $timestamps = false;
+
+    /**
+     * @var string The database table used by the model.
+     */
+    public $table = 'adrisonluz_nanocms_forms';
+
+    public function getPaginaIdOptions(){
+        return Pagina::lists('titulo','id');
+    }
+}
