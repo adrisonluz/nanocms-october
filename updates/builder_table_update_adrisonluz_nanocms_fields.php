4d773544715473518fad25a4358a5386cd026034
Adrison Luz <aluz@reweb.com.br>
2017-01-22 17:54:31 -0200

    Ajustes emm Fields e Forms

---
+++ updates/builder_table_update_adrisonluz_nanocms_fields.php
@@ -0,0 +1,23 @@
+<?php namespace AdrisonLuz\NanoCms\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdateAdrisonluzNanocmsFields extends Migration
+{
+    public function up()
+    {
+        Schema::table('adrisonluz_nanocms_fields', function($table)
+        {
+            $table->dropColumn('form_id');
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('adrisonluz_nanocms_fields', function($table)
+        {
+            $table->integer('form_id')->unsigned();
+        });
+    }
+}
