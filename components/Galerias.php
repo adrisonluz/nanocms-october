24bd3dc8c522e1f971cf36725dd44deb314ec296
Adrison Luz <aluz@reweb.com.br>
2017-02-20 15:47:27 -0300

    Ajuste componente Galerias

--- components/Galerias.php
+++ components/Galerias.php
@@ -1,6 +1,8 @@
 <?php namespace AdrisonLuz\NanoCms\Components;
 
 use Cms\Classes\ComponentBase;
+use Cms\Classes\Page;
+use AdrisonLuz\NanoCms\Models\Galeria;
 
 class Galerias extends ComponentBase
 {
@@ -8,33 +10,47 @@ class Galerias extends ComponentBase
     public function componentDetails()
     {
         return [
-            'name'        => 'Galerias Component',
-            'description' => 'No description provided yet...'
+            'name'        => 'Galerias',
+            'description' => 'Lista todas as galerias cadastradas.'
         ];
     }
 
     public function defineProperties()
-      {
-          return [
-            'debug' => [
-                 'title'             => 'Debug',
-                 'description'       => 'Permite debugar o componente.',
-                 'default'           => false,
-                 'type'              => 'checkbox',
+    {
+        return [
+          'debug' => [
+               'title'             => 'Debug',
+               'description'       => 'Permite debugar o componente.',
+               'default'           => false,
+               'type'              => 'checkbox',
+          ],
+            'page' => [
+                'title' => 'Página interna',
+                'description' => 'Define a página de interna de galerias',
+                'type' => 'dropdown',
             ],
           ];
       }
 
-      public function onRun()
-      {
-          $categorias = Categoria::all();
+    public function getPageOptions()
+    {
+        $paginas = Page::sortBy('baseFileName')->lists('baseFileName', 'baseFileName');
+        $paginas[0] = 'Nenhuma';
+        
+        return $paginas;
+    }
 
-          $this->page['categorias'] = $categorias;
+    public function onRun()
+    {
+        $galerias = Galeria::ativos();
 
-          // Debug
-          if($this->property('debug') == 1){
-              dd(Categoria::all()->toArray());
-          }
-      }
+        $this->page['galerias'] = $galerias;
+        $this->page['page_slug'] = $this->property('page');
+
+        // Debug
+        if($this->property('debug') == 1){
+            dd($galerias->toArray());
+        }
+    }
 
 }
--- models/Galeria.php
+++ models/Galeria.php
@@ -20,9 +20,18 @@ class Galeria extends Model
      * Remove this line if timestamps are defined in the database table.
      */
     public $timestamps = false;
+    
+    public $attachMany = [
+        'imagens' => ['System\Models\File']
+    ];
 
     /**
      * @var string The database table used by the model.
      */
     public $table = 'adrisonluz_nanocms_galerias';
+    
+    public function scopeAtivos($query)
+    {
+      return $query->where('ativo','=',1)->get();
+    }
 }
