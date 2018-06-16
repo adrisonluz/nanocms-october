1e9752182b35bb025eec38cd1d81d6cc76ae95c5
root <root@ip-172-31-2-172.sa-east-1.compute.internal>
2018-06-16 01:30:10 +0000

    Renomeado tabela de envios para envios_campos

---
+++ updates/builder_table_update_adrisonluz_nanocms_envios_campos.php
@@ -0,0 +1,17 @@
+<?php namespace AdrisonLuz\NanoCms\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdateAdrisonluzNanocmsEnviosCampos extends Migration
+{
+    public function up()
+    {
+        Schema::rename('adrisonluz_nanocms_envios', 'adrisonluz_nanocms_envios_campos');
+    }
+    
+    public function down()
+    {
+        Schema::rename('adrisonluz_nanocms_envios_campos', 'adrisonluz_nanocms_envios');
+    }
+}
