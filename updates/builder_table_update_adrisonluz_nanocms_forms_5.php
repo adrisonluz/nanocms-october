777e8c22940743be5516da74a54bdac9ea013b20
root <root@ip-172-31-2-172.sa-east-1.compute.internal>
2018-08-13 23:49:52 +0000

    Adicionado campo para função de callback em formulários

---
+++ updates/builder_table_update_adrisonluz_nanocms_forms_5.php
@@ -0,0 +1,25 @@
+<?php namespace AdrisonLuz\NanoCms\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdateAdrisonluzNanocmsForms5 extends Migration
+{
+    public function up()
+    {
+        Schema::table('adrisonluz_nanocms_forms', function($table)
+        {
+            $table->string('callback')->nullable();
+            $table->integer('ordem')->default(1)->change();
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('adrisonluz_nanocms_forms', function($table)
+        {
+            $table->dropColumn('callback');
+            $table->integer('ordem')->default(NULL)->change();
+        });
+    }
+}
